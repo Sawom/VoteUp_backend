ba7412bc3e406039ba4d09bf923dcafeb9150d50
Sawom <asawom250@gmail.com>
2026-04-18 23:46:20 +0600

    create user logic

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,28 @@
-import { User } from "@prisma/client";
+import { Role, User } from "@prisma/client";
+import { IFile } from "../../interfaces/file";
+import * as bcrypt from "bcrypt";
+import { Request } from "express";
+import prisma from "../../../shared/prisma";
 
-const createAdmin = async (req: Request): Promise<User> => {
-  return 0 as unknown as User;
+const createUser = async (req: Request): Promise<User> => {
+  // const file = req.file as IFile;
+
+  const hashedPassword: string = await bcrypt.hash(req.body.password, 12);
+
+  const userData = {
+    email: req.body.admin.email,
+    name: req.body.name,
+    password: hashedPassword,
+    role: Role.USER,
+  };
+
+  const createUser = await prisma.user.create({
+    data: userData,
+  });
+
+  return createUser;
 };
 
 export const userService = {
-  createAdmin,
+  createUser,
 };
