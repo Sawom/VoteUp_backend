5a604a9284c215b95c8beaf0392ae6615d8dea3c
Sawom <asawom250@gmail.com>
2026-04-20 08:14:26 +0600

    cloudenary setup with multer

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,11 @@ import path from 'path';
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
-    node_env: process.env.NODE_ENV,
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL,
-}
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
+};
--- src/helpars/fileUploader.ts
+++ src/helpars/fileUploader.ts
@@ -1,9 +1,60 @@
-import dotenv from "dotenv";
-import { v2 as cloudinary } from "cloudinary";
+import {
+  UploadApiErrorResponse,
+  UploadApiResponse,
+  v2 as cloudinary,
+} from "cloudinary";
+import { ICloudinaryResponse, IFile } from "../app/interfaces/file";
 import fs from "fs";
+import multer from "multer";
+import path from "path";
+import config from "../config/index";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: config.cloudinary_cloud_name,
+  api_key: config.cloudinary_api_key,
+  api_secret: config.cloudinary_api_secret,
 });
+
+// create a uploads folder on root.
+// if folder does not exists then create it when server is start to avoid *ENOENT* error
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(process.cwd(), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const uploadToCloudinary = async (
+  file: IFile,
+): Promise<ICloudinaryResponse | undefined> => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      file.path,
+      {
+        folder: "vote_up_project",
+        use_filename: true,
+      },
+
+      (
+        error: UploadApiErrorResponse | undefined,
+        result: UploadApiResponse | undefined,
+      ) => {
+        fs.unlinkSync(file.path);
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result as unknown as ICloudinaryResponse);
+        }
+      },
+    );
+  });
+};
+
+export const fileUploader = {
+  upload,
+  uploadToCloudinary,
+};
