e72c7380b8cba411092be5c75a896ce69b74a889
Sawom <asawom250@gmail.com>
2026-04-25 02:41:44 +0600

    jwt nodemailer setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,4 +11,13 @@ export default {
   cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
   cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
+
+  jwt: {
+    jwt_secret: process.env.JWT_SECRET,
+    expires_in: process.env.EXPIRES_IN,
+    refresh_token_secret: process.env.REFRESH_TOKEN_SECRET,
+    refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN,
+    reset_pass_secret: process.env.RESET_PASS_TOKEN,
+    reset_pass_token_expires_in: process.env.RESET_PASS_TOKEN_EXPIRES_IN,
+  }
 };
